2f556083b2db4c35368bea8f63c45c248d99e3f0
María Pérez <123765659+mariaperez15@users.noreply.github.com>
2023-12-25 16:52:40 +0100

    buscador funciona todo aunque mejor pasar el search a otro componente

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FilmsApiService } from 'src/app/services/films-api.service';
 import { Film } from 'src/app/interfaces/interfaces';
 
 @Component({
@@ -10,31 +9,28 @@ import { Film } from 'src/app/interfaces/interfaces';
 export class SearchComponent  implements OnInit {
 
   @Input() films: Film[] = [];
-  public results : Film[] = [];
+  @Input() enFavoritos: boolean = false;
+  public results = [...this.films];
+  isSearching: boolean = false;
 
-  constructor(private filmsApi: FilmsApiService) {
-    console.log(this.films);
-   }
+  constructor() { }
 
   ngOnInit() {}
-
-  // searchFilm(event:any){
-  //   const query = event.target.value.toLowerCase();
-  //   this.results = this.films.filter((film: Film) => 
-  //   film.original_title && film.original_title.toLowerCase().indexOf(query) > -1);
-  //   console.log("search ejecutandose")
-  //   console.log("estas buscando: " +  query)
-  // }
-
   searchFilm(event: any) {
     console.log(this.films);
-    const query = event.target.value.toLowerCase();
+    const query = event.target.value.trim().toLowerCase();
+    this.isSearching = true;
+    console.log("isSearching:", this.isSearching);
     this.results = this.films.filter((film: Film) => {
         const titleToSearch = film.original_title ? film.original_title.toLowerCase() : (film.name ? film.name.toLowerCase() : '');
         return titleToSearch.includes(query);
     });
+    console.log("Results después de la búsqueda:", this.results);
     console.log("search ejecutandose")
     console.log("estas buscando: " +  query)
     console.log(this.results)
+
 }
+
+
 }
